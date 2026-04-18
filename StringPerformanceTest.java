e684d0472c8a0671724388c2de8049213e839c92
MADHUMATIBS <madhumathibs386@gmail.com>
2026-04-19 01:44:06 +0530

    Add files via upload

---
+++ StringPerformanceTest.java
@@ -0,0 +1,23 @@
+package String_Functions;
+public class StringPerformanceTest{  
+	  public static void main(String[] args){  
+	      long startTime = System.currentTimeMillis();  
+	      StringBuffer sb = new StringBuffer("Java");  
+	      for (int i=0; i<10000; i++){  
+	          sb.append("Tpoint");  
+	      }  
+	      System.out.println("Time taken by StringBuffer: " + (System.currentTimeMillis() - startTime) + "ms");  
+	      startTime = System.currentTimeMillis();  
+	      StringBuilder sb2 = new StringBuilder("Java");  
+	      for (int i=0; i<10000; i++){  
+	          sb2.append("Tpoint");  
+	      }  
+	      System.out.println("Time taken by StringBuilder: " + (System.currentTimeMillis() - startTime) + "ms");  
+	  } }
+
+/*StringBuffer
+Thread-safe (synchronized)
+Slower due to synchronization overhead
+StringBuilder
+Not thread-safe
+Faster because it does not use synchronization */
