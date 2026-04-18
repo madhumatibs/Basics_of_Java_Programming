65d625a6a2e4dc09e7f63f3167fb3b5ccd469162
MADHUMATIBS <madhumathibs386@gmail.com>
2026-04-19 00:49:03 +0530

    Add files via upload

---
+++ c1.java
@@ -0,0 +1,19 @@
+package ListInterface;
+import java.util.*;
+public class c1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String>	ls = new LinkedList<>();
+		ls.add("Orange");
+		ls.add("geen");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+		Iterator iterator = ls.listIterator(2);
+		while (iterator.hasNext()) {
+		System.out.println(iterator.next());
+		}
+	}
+
+}
