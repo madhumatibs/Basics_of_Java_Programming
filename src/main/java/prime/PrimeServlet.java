b35f31c3d8a596093efa76656e652b771e537a51
madhumatibs <madhumathibs386@gmail.com>
2026-04-19 21:05:35 +0530

    commit

---
+++ src/main/java/prime/PrimeServlet.java
@@ -0,0 +1,44 @@
+package prime;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+/* 6c. Servlet program to check prime number */
+
+@WebServlet("/prime")
+public class PrimeServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        int num = Integer.parseInt(request.getParameter("num"));
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        out.println("<html><body>");
+        out.println("<h2>Prime Number Result</h2>");
+
+        if (isPrime) {
+            out.println("<p>" + num + " is a PRIME number</p>");
+        } else {
+            out.println("<p>" + num + " is NOT a prime number</p>");
+        }
+
+        out.println("</body></html>");
+    }
+}
